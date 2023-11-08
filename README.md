fb184c76762d4b2a7049b7ca7e0b8bf2be9c1867
Daniel Felix <felixdaniel9@gmail.com>
2023-11-08 17:29:36 -0300

    Add README and font awesome icon

---
+++ README.md
@@ -0,0 +1,20 @@
+# Slider Project in JavaScript
+
+Este é um projeto simples que implementa um slider de imagens usando HTML, CSS e JavaScript. O objetivo deste projeto é demonstrar como criar um slider básico que pode ser usado em sites para exibir imagens de forma dinâmica e interativa.
+
+## Funcionalidades
+
+- Navegação entre as imagens usando botões "Anterior" e "Próximo".
+- Exibição de legendas para cada imagem.
+- Controle de autoplay, permitindo a reprodução automática das imagens.
+
+## Pré-requisitos
+
+Para executar este projeto em sua máquina local, você precisará de um navegador da web moderno que suporte JavaScript. Não há necessidade de instalar nenhuma dependência adicional.
+
+## Instalação
+
+1. Clone este repositório para a sua máquina local:
+
+```bash
+git clone https://github.com/danielfelix45/slider-project-javascript.git
--- script.js
+++ script.js
@@ -1,30 +1,37 @@
+// Declaração de variáveis
 let totalSlides = document.querySelectorAll('.slider--item').length;
 let currentSlide = 0;
 
-document.querySelector('.slider--width').style.width =
-    `calc(100vw * ${totalSlides})`;
-document.querySelector('.slider--controls').style.height =
-    `${document.querySelector('.slider').clientHeight}px`;
+// Configuração inicial do slider
+const sliderWidth = document.querySelector('.slider--width');
+const sliderControls = document.querySelector('.slider--controls');
+sliderWidth.style.width = `calc(100vw * ${totalSlides})`;
+sliderControls.style.height = `${document.querySelector('.slider').clientHeight}px`;
 
+// Função para ir para o slide anterior
 function goPrev() {
-    currentSlide--;
-    if(currentSlide < 0) {
-        currentSlide = totalSlides - 1;
-    }
-    updateMargin();
-}
-function goNext() {
-    currentSlide++;
-    if(currentSlide > (totalSlides-1)) {
-        currentSlide = 0;
-    }
-    updateMargin();
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = totalSlides - 1;
+  }
+  updateMargin();
 }
 
-function updateMargin() {
-    let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
-    let newMargin = (currentSlide * sliderItemWidth);
-    document.querySelector('.slider--width').style.marginLeft =
-        `-${newMargin}px`;
+// Função para ir para o próximo slide
+function goNext() {
+  currentSlide++;
+  if (currentSlide >= totalSlides) {
+    currentSlide = 0;
+  }
+  updateMargin();
 }
-setInterval(goNext, 4000);
+
+// Função para atualizar a margem do slider
+function updateMargin() {
+  const sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+  const newMargin = currentSlide * sliderItemWidth;
+  sliderWidth.style.marginLeft = `-${newMargin}px`;
+}
+
+// Iniciar o slider automaticamente a cada 4 segundos
+setInterval(goNext, 5000);
